dfd285f02bf669ba6fe7c06f4bb91749bf5f56f0
ssongmo <heosongmoo@naver.com>
2017-03-21 11:54:04 +0900

    sharedPreference_savedEmail

--- app/src/main/java/com/example/goodgoodman/sharedpreference/MainActivity.java
+++ app/src/main/java/com/example/goodgoodman/sharedpreference/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.goodgoodman.sharedpreference;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         if("false".equals(propertyUtil.getProperty("firstOpen"))){
             layout.setVisibility(View.GONE);
         }
+
+        loadSetting();  //세팅된 값을 가져와 뿌린다.
+
     }
 
     public void closeHelp(View view){
@@ -40,7 +45,25 @@ public class MainActivity extends AppCompatActivity {
         propertyUtil.saveProperty("firstOpen", "false");
     }
     public void saveSetting(View view){
+        //1. preference 생성하기
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        //2. SharedPreference에 값을 입력하기 위해서는 에디터를 통해서만 가능.
+        SharedPreferences.Editor editor = sharedPref.edit();
+        //editor.putInt("키", "값");
+        editor.putString("email", editName.getText().toString());
+        editor.putBoolean("onOff", switchOnOff.isChecked());
+        //입력된 값을 반영.
+        editor.commit();
 
+    }
+    public void loadSetting(){
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+
+        String email = sharedPref.getString("email", null);
+        boolean onOff = sharedPref.getBoolean("onOff", false);
+
+        editName.setText(email);
+        switchOnOff.setChecked(onOff);
     }
 
 
